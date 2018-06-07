72b76e2c877a6e8304d9e5926ae4b09452b81c0e
AndreaVale <lain_iwakura55@hotmail.com>
2018-06-07 11:06:25 -0500

    primera subida del ejercicio de cifrado césar

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,13 @@
 window.cipher = {
-  // ...
 };
+
+const cifrar= () => {
+  let mensajeACifrar = document.getElementById("mensaje-cifrar").value;
+  //document.getElementById("mensaje-cifrado").innerHTML = mensajeACifrar;
+  for (let numb = 0; numb<mensajeACifrar.lenght; numb++){
+  let menCifrado = mensajeACifrar.chartCodeAt(numb);
+  let menChingon = (mensajeACifrar-65+33)%menCifrado+65;
+  document.getElementById("mensaje-cifrado").innerHTML = menChingon;
+
+  }
+}
