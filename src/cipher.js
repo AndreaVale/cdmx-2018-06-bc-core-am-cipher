5d0166499eda821370173fc512e20244f8346391
AndreaVale <lain_iwakura55@hotmail.com>
2018-06-07 19:44:36 -0500

    segunda subida de archivos. Cifrado OK

--- src/cipher.js
+++ src/cipher.js
@@ -1,13 +1,20 @@
-window.cipher = {
-};
+function cifrar(cifrar){
+let mensajeSalida = "";
+let mensajeCifrar = document.getElementById('entrada').value.toUpperCase();
+let saltos = parseInt(document.getElementById('saltos').value);
+//console.log('entrada');
+//console.log('saltos');
+for (let i = 0; i < mensajeCifrar.length; i++){
+let mensajeEntrada = mensajeCifrar.charCodeAt(i);
+//console.log(mensajeEntrada);
+let mensajeBueno = (mensajeEntrada-65+saltos)%26+65;
+mensajeSalida += String.fromCharCode(mensajeBueno);
+console.log(mensajeSalida);
+/*let mensajeSalida = String.fromCharCode(mensajeEntrada);
+console.log(mensajeSalida);
+let mensajeBueno = (mensajeSalida-65+saltos)%26+65;
+console.log(mensajeBueno);*/
+document.getElementById("resultado").innerHTML=mensajeSalida;
+}
 
-const cifrar= () => {
-  let mensajeACifrar = document.getElementById("mensaje-cifrar").value;
-  //document.getElementById("mensaje-cifrado").innerHTML = mensajeACifrar;
-  for (let numb = 0; numb<mensajeACifrar.lenght; numb++){
-  let menCifrado = mensajeACifrar.chartCodeAt(numb);
-  let menChingon = (mensajeACifrar-65+33)%menCifrado+65;
-  document.getElementById("mensaje-cifrado").innerHTML = menChingon;
-
-  }
 }
