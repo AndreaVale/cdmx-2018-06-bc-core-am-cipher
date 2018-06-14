d52f485471d07ada7f43c973b3bfa0a9483cceb6
AndreaVale <lain_iwakura55@hotmail.com>
2018-06-14 16:02:34 -0500

    primera función del cipher ya corre...que chingon

--- src/cipher.js
+++ src/cipher.js
@@ -1,12 +1,15 @@
-function botonC(){
-  cifrado.style.display="block";
-  }
-function cifrar(){
+//function botonC(){
+  //cifrado.style.display="block";
+  //}
+  window.cipher ={
+
+encode:(mensajeCifrar,saltos)=>{
+console.log(saltos);
 let mensajeSalida = " ";
-let mensajeCifrar = document.getElementById('entrada').value.toUpperCase();
-let saltos = parseInt(document.getElementById('saltos').value);
 //console.log('entrada');
 //console.log('saltos');
+mensajeCifrar = mensajeCifrar.toUpperCase();
+saltos = parseInt(saltos);
 for (let i = 0; i < mensajeCifrar.length; i++){
 let mensajeEntrada = mensajeCifrar.charCodeAt(i);
 
@@ -22,48 +25,45 @@ let mensajeBueno = (mensajeEntrada-65+saltos)%26+65;
 //console.log(mensajeBueno);
 mensajeSalida += String.fromCharCode(mensajeBueno);
 console.log(mensajeSalida);
-/*let mensajeSalida = String.fromCharCode(mensajeEntrada);
-console.log(mensajeSalida);
-let mensajeBueno = (mensajeSalida-65+saltos)%26+65;
-console.log(mensajeBueno);*/
- cifradoRespuesta.style.display="block";
+ //cifradoRespuesta.style.display="block";
 document.getElementById("resultado").innerHTML=mensajeSalida;
 }
 }
 }
+}
 //toda la función del decifrado
-function botonD(){
-  decifrado.style.display="block";}
-function decifrar(){
+//function botonD(){
+  //decifrado.style.display="block";}
+//function decifrar(){
   //Llamando del HTML al JS
-let mensajeDecifrar = document.getElementById('entrada2').value.toUpperCase();
-let saltosDos = parseInt(document.getElementById('saltos2').value);
-let mensajeSalidaDos = "";
+//let mensajeDecifrar = document.getElementById('entrada2').value.toUpperCase();
+//let saltosDos = parseInt(document.getElementById('saltos2').value);
+//let mensajeSalidaDos = "";
 //Consologueando para ver si me da lo que le pido
 //console.log(mensajeDecifrar);
 //console.log(saltosDos);
 //Este for es para el decifrado
-for (var numb = 0; numb < mensajeDecifrar.length; numb++){
+//for (var numb = 0; numb < mensajeDecifrar.length; numb++){
 //Transformando el mensaje cifrado a ASCII
-var mensajePerritos = mensajeDecifrar.charCodeAt(numb);
+//var mensajePerritos = mensajeDecifrar.charCodeAt(numb);
 //Viendo si efectivamente me sale el mensaje en ASCII
 //console.log(mensajePerritos);
 
-if (mensajePerritos==32) {
-  mensajeSalidaDos+=String.fromCharCode(mensajePerritos);
-  document.getElementById('resultado-dos').innerHTML = mensajeSalidaDos;
-  console.log(mensajeSalidaDos);
-} else {
+//if (mensajePerritos==32) {
+  //mensajeSalidaDos+=String.fromCharCode(mensajePerritos);
+  //document.getElementById('resultado-dos').innerHTML = mensajeSalidaDos;
+  //console.log(mensajeSalidaDos);
+//} else {
 
 // Fórmula para descencriptar
-var mensajeBuenoDos = (mensajePerritos-90-saltosDos)%26+90;
-console.log(mensajeBuenoDos);
+//var mensajeBuenoDos = (mensajePerritos-90-saltosDos)%26+90;
+//console.log(mensajeBuenoDos);
 //Transformando el ASCII de nuevo a letras (con el mensaje decifrado)
-mensajeSalidaDos += String.fromCharCode(mensajeBuenoDos);
-console.log(mensajeSalidaDos);
+//mensajeSalidaDos += String.fromCharCode(mensajeBuenoDos);
+//console.log(mensajeSalidaDos);
 //Imprimiendo en pantalla
-decifradoRespuesta.style.display= "block";
-document.getElementById('resultado-dos').innerHTML=mensajeSalidaDos;
-}
-}
-}
+//decifradoRespuesta.style.display= "block";
+//document.getElementById('resultado-dos').innerHTML=mensajeSalidaDos;
+//}
+//}
+//}
---
+++ src/index.js
@@ -0,0 +1,7 @@
+let mensajeCifrar = document.getElementById('entrada');
+let saltos = document.getElementById('saltos');
+let boton = document.getElementById('botonentrada');
+
+//botonentrada.addEventListener("click", event =>{
+  //window.cipher.encode(mensajeCifrar.value,saltos.value);
+//})
